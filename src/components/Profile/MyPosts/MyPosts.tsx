ff91af3a6f69fb06ff4dceb819b773b9e92747ce
gubin_km <xhunty@gmail.com>
2020-01-14 20:46:21 +0300

    d4

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -5,7 +5,8 @@ import Post from './Post/Post';
 const MyPosts: React.FC = () =>{
     return(   
         <div className={css.posts}>my posts
-            <Post />
+            <Post message='Hi, how are you?' likes={14}/>
+            <Post message='Its my first post' likes={123}/>
         </div>)
 }
 export default MyPosts;
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import css from './Post.module.css'
 
-const Post: React.FC = () =>{
+interface IPostProps{
+    message:string
+    likes:number
+}
+const Post: React.FC<IPostProps> = (props) =>{
+    const {message,likes} = {...props};
     return(
-        <div className={css.item}>post 1
-            <img src='https://amp.spark.ru/public/img/user_ava_big.png'></img>
+        <div className={css.item}>
+            <div>
+                <img src='https://amp.spark.ru/public/img/user_ava_big.png'></img>
+            </div>
+            <div>{message}</div>
+            <span>likes:{likes}</span>
         </div>
     )
 }
